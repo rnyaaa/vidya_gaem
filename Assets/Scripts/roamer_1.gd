2770eb14e54d43c8fd9216f0fe6b774aeb570df9
musarrindill <atli.finn@gmail.com>
2025-03-15 17:22:49 +0000

    roamer_update

---
+++ Assets/Scripts/roamer_1.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+var startPos: Vector3
+var updatedPos: Vector3
+@export var roamRange: int = 2
+@export var roamIntervalFrames: int = 360
+var randomizedVector: Vector3
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	startPos = global_position
+	print(startPos)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:	
+	updatedPos = startPos + randomizedVector
+	global_position = global_position.lerp(updatedPos, delta * 0.5)
+	
+	if Engine.get_frames_drawn() % roamIntervalFrames == 0:
+		randomDir()
+	pass
+
+func randomDir():
+	randomizedVector.x = randf_range(-roamRange,roamRange)
+	randomizedVector.z = randf_range(-roamRange,roamRange)
+	
